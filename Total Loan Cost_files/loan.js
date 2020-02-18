db90f987cdc24e52fca0a1cccec0fae0a538498d
unstoppableme5 <jacobhall277@gmail.com>
2020-02-17 21:12:50 -0600

    HO4

---
+++ Total Loan Cost_files/loan.js
@@ -0,0 +1,27 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+
+/*
+ *start
+   declare floatMonthlyPayment, intLoanTerm, floatTotalCost
+   get floatMonthlyPayment
+   get intLoanTerm
+   calculate floatTotalCost = floatMonthlyPayment * intLoanTerm
+   output floatTotalCost
+  end
+*/
+ var calculate = function()
+ {
+    $("total_cost").value = $("loan_length").value * $("monthly_payment").value;
+ }
+ 
+ window.onload = function () 
+{
+    $("loan_length").value = "";
+    $("total_cost").value = "";
+    $("calc").onclick = calculate;
+    $("monthly_payment").focus();
+}
