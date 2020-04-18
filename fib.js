1b995e8c5d241872df367d528e36d6572573a61e
unstoppableme5 <55761654+unstoppableme5@users.noreply.github.com>
2020-04-17 19:19:55 -0500

    Update fib.js

--- fib.js
+++ fib.js
@@ -1,10 +1,22 @@
 function getFib(){
     //get the number we want to calculate
     var intNum = parseInt(document.getElementById("total_fib").value);
+    document.getElementById("output").innerText = "";
+    var strFibNumbers = "";
+
+    while(intNum > 0){
+        strFibNumbers += calcFib(intNum) + " ";
+        
+        intNum--;
+    }
+    document.getElementById("output").innerText = strFibNumbers;
+}
+
+function calcFib(intNum){
     //calculate phi
     floatPhi = (Math.sqrt(5) + 1) / 2;
     //calculate the fibinaci sequence
     floatFib = (Math.pow(floatPhi, intNum) - Math.pow(-1 * floatPhi, -1 * intNum)) / Math.sqrt(5);
     //update dom
-    document.getElementById("output").value = Math.round(floatFib);
+    return Math.round(floatFib);
 }
